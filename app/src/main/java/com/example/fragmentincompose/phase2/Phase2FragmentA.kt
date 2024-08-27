2721f953a1164e6dcdedcee5c2b3d99b2326d296
JiaChian <jiachian.du@pinkoi.com>
2024-08-27 12:50:49 +0800

    phase2 - create the compose view

---
+++ app/src/main/java/com/example/fragmentincompose/phase2/Phase2FragmentA.kt
@@ -0,0 +1,32 @@
+package com.example.fragmentincompose.phase2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+
+class Phase2FragmentA : Fragment() {
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    return ComposeView(requireContext()).apply {
+      setViewCompositionStrategy(
+        ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+      )
+      setContent {
+        Box(
+          modifier = Modifier.fillMaxSize(),
+          contentAlignment = Alignment.Center,
+        ) {
+          Text(text = "A")
+        }
+      }
+    }
+  }
+}

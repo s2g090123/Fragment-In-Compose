6e13c5ac4026c5eb6eed1c8e6142a2bf1f519b64
JiaChian <jiachian.du@pinkoi.com>
2024-08-27 12:39:02 +0800

    phase1 - import dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,6 @@ dependencies {
   androidTestImplementation(libs.androidx.ui.test.junit4)
   debugImplementation(libs.androidx.ui.tooling)
   debugImplementation(libs.androidx.ui.test.manifest)
+
+  implementation(libs.androidx.fragment.compose)
 }
